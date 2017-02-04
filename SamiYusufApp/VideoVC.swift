c3c954d2e71782d5b4134364d0ee3f786db0c7e1
mshayanh13 <mohammadhemani@gmail.com>
2017-02-03 18:03:00 -0600

    Added back button to VideoVC

--- SamiYusufApp/VideoVC.swift
+++ SamiYusufApp/VideoVC.swift
@@ -35,6 +35,9 @@ class VideoVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation
